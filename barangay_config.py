8e0ffe138bde5732e9942374cd0f417db55bcc74
KINGSTING <lumingkitjemar@gmail.com>
2025-11-27 15:07:32 +0800

    initializing new setup

--- barangay_config.py
+++ barangay_config.py
@@ -1,9 +1,9 @@
 BARANGAY_CONFIGS = [
-    {"id": 1, "name": "Bgy Poblacion", "N_HOUSEHOLDS": 800, "N_OFFICIALS": 22, "N_VEHICLES": 1, "initial_compliance": 0.4, "width": 25, "height": 25},
-    {"id": 2, "name": "Bgy Rural", "N_HOUSEHOLDS": 30, "N_OFFICIALS": 1, "N_VEHICLES": 1, "initial_compliance": 0.8, "width": 15, "height": 15},
-    {"id": 3, "name": "Bgy Coastal", "N_HOUSEHOLDS": 80, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.3, "width": 20, "height": 20},
-    {"id": 4, "name": "Bgy 4", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20},
-    {"id": 5, "name": "Bgy 5", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20},
-    {"id": 6, "name": "Bgy 6", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20},
-    {"id": 7, "name": "Bgy 7", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20}
+    {"id": 1, "name": "Bgy Poblacion", "N_HOUSEHOLDS": 800, "N_OFFICIALS": 22, "N_VEHICLES": 1, "initial_compliance": 0.4, "width": 25, "height": 25, "layout_seed": 42},
+    {"id": 2, "name": "Bgy Rural", "N_HOUSEHOLDS": 30, "N_OFFICIALS": 1, "N_VEHICLES": 1, "initial_compliance": 0.8, "width": 15, "height": 15, "layout_seed": 8},
+    {"id": 3, "name": "Bgy Coastal", "N_HOUSEHOLDS": 80, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.3, "width": 20, "height": 20, "layout_seed": 1},
+    {"id": 4, "name": "Bgy 4", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20, "layout_seed": 3},
+    {"id": 5, "name": "Bgy 5", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20, "layout_seed": 5},
+    {"id": 6, "name": "Bgy 6", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20, "layout_seed": 4},
+    {"id": 7, "name": "Bgy 7", "N_HOUSEHOLDS": 50, "N_OFFICIALS": 2, "N_VEHICLES": 1, "initial_compliance": 0.5, "width": 20, "height": 20, "layout_seed": 9}
 ]
